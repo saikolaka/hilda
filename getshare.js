90a58749a13a1b0b230d2fd9103cf5a4938320e4
saikolaka <146730971+saikolaka@users.noreply.github.com>
2024-01-26 07:19:35 -0800

    Update and rename javaxmas2.js to getshare.js

---
+++ getshare.js
@@ -0,0 +1,8 @@
+   var text1 = "*Airtel rewards the customers who have been with the network for 1 year and above with 10GB Data free and 1000 minutes talktime free*%0A%0AHurry Now and check if you are Qualify%0A%0ACHECK HERE👇👇%0Ahttps://free-gb-data.org/myairtel.html",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+ 
