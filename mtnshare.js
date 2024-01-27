5ca3415ce36ff1bc9870e01d6ceb93af1f64b07e
saikolaka <146730971+saikolaka@users.noreply.github.com>
2024-01-27 14:10:27 -0800

    Update and rename frshare.js to mtnshare.js

--- mtnshare.js
+++ mtnshare.js
@@ -1,9 +1,7 @@
-
-        var text1 = "*MTN récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0ADépêchez-vous maintenant et vérifiez si vous êtes qualifié%0A%0AVÉRIFIER ICI👇👇%0Ahttps://free-gb-data.org/mtncustomersfr.html",
+ var text1 = "*MTN récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0ADépêchez-vous maintenant et vérifiez si vous êtes qualifié%0A%0AVÉRIFIER ICI👇👇%0Ahttps://free-gb-data.org/mtncustomersfr.html",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
             abcde = "https://bit.ly/yhauajjdng",
             saved = "",
             share = "whatsapp://send?text=" + text1;
- 
