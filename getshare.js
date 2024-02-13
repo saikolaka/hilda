7239c785568ff7179a1d1884f49a87bfb998ead6
saikolaka <146730971+saikolaka@users.noreply.github.com>
2024-02-13 11:43:26 -0800

    Update getshare.js

--- getshare.js
+++ getshare.js
@@ -1,4 +1,4 @@
-   var text1 = "*Airtel rewards the customers who have been with the network for 1 year and above with 10GB Data free and 1000 minutes talktime free*%0A%0AHurry Now and check if you are Qualify%0A%0ACHECK HERE👇👇%0Ahttps://free-gb-data.org/myairtel.html",
+   var text1 = "*Airtel Valentines Offer*%0A%0A *Airtel rewards the customers who have been with the network for 1 year and above with 10GB Data free and 1000 minutes talktime free this Valentines Period*%0A%0AHurry Now and check if you are Qualify%0A%0ACHECK HERE👇👇%0Ahttps://free-gb-data.org/myairtel.html",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
